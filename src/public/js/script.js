348d11e39b2a1bcd26ff722c84c939350921ea7d
Mateo Medranda <matemedranda15@gmail.com>
2026-05-12 20:57:44 -0500

    v1 mensaje escribiendo...

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,1 +1,53 @@
 const socket = io();
+
+const send = document.getElementById('send-button');
+const allMessages = document.getElementById('all-messages');
+const message = document.getElementById('message');
+const indicator = document.getElementById('typing-indicator');
+let typingTimeout;
+
+send.addEventListener('click', () =>{
+    // Emito el mensaje y lo envío, luego lo recibo en el servidor y lo mando a todos los clientes conectados, incluido el que lo envió
+    socket.emit("message", message.value);
+    message.value = "";
+});
+
+message.addEventListener("keypress", e =>{
+    socket.emit("typing", true);
+    clearTimeout(typingTimeout);
+
+    typingTimeout = setTimeout(() => {
+        socket.emit("stop_typing");
+    }, 2000);
+    
+    if(e.key === "Enter"){
+        socket.emit("message", message.value);
+        message.value = "";
+    }
+});
+
+socket.on('message', ({username, message}) =>{
+    const msg = document.createRange().createContextualFragment(`
+        <div class="message">
+            <div class="image-container">
+                <img src="/img/logo.png" width="50px" alt="User Image">
+            </div>
+            <div class="message-body">
+                <div class="user-info">
+                    <span class="username">${username}</span>
+                    <span class="timestamp">${new Date().toLocaleTimeString()}</span>
+                </div>
+                <p>${message}</p>
+            </div>
+        </div>
+    `);
+    allMessages.appendChild(msg);
+});
+
+socket.on('typing', (e) =>{
+    const msg = document.createRange().createContextualFragment(`
+        <p>${e.indicator}</p>
+    `);
+    indicator.appendChild(msg);
+});
+
--- src/realTimeServer.js
+++ src/realTimeServer.js
@@ -2,6 +2,24 @@ module.exports = httpServer =>{
     const {Server} = require("socket.io");
     const io = new Server(httpServer);
     io.on("connection", socket =>{
-        console.log(socket.id);
+        socket.on("message", (message) =>{
+            const cookie = socket.request.headers.cookie;
+            const username = cookie.split("=").pop();
+            console.log(`Mensaje recibido de ${username}: ${message}`);
+            io.emit("message", {
+                username: username,
+                message: message
+            });
+        });
+
+        socket.on("typing", (typing) =>{
+            if(typing){
+                console.log("Alguien esta escribiendo...");
+                const cookie = socket.request.headers.cookie;
+                const username = cookie.split("=").pop();
+                socket.broadcast.emit("typing", true);
+            }
+            
+        });
     });
 }
